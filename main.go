f1460df778b1cd007d7ff7a831ccb2b945772262
pengzhimou <warscain@163.com>
2020-04-27 23:52:12 +0800

    v1

--- main.go
+++ main.go
@@ -6,7 +6,6 @@ import (
 	. "ospback/backup"
 	"ospback/logger"
 	"sync"
-	"time"
 )
 
 func getKeys(m map[string]LinuxOS) []string {
@@ -17,7 +16,7 @@ func getKeys(m map[string]LinuxOS) []string {
 	}
 	return keys
 }
-func linuxNfsBackup(gorlimit int) {
+func linuxNfsBackupLimit(gorlimit int) {
 	bksrvyml, err := ReadBKServerYaml("etc/backup.yml")
 	if err != nil {
 		errors.New("yaml fmt not correct!")
@@ -31,73 +30,88 @@ func linuxNfsBackup(gorlimit int) {
 	}
 
 	data := srvyml.LinuxOsConfig
-	keys := getKeys(data)
-	gorNum := 0
-	gorLimit := gorlimit
 	wg := sync.WaitGroup{}
+	ch := make(chan bool, gorlimit)
 
-	for i := 0; i < len(keys); i++ {
+	for k, v := range data {
 		wg.Add(1)
-		if gorNum < gorLimit {
-			gorNum++
-			go func(n int) {
-				fmt.Println(data[keys[i]])
-				server := data[keys[i]]
-				/////////////////////
-				servertmp := LinuxOS{
-					Ip: server.Ip,
-					Conntp: Conntype{
-						SshCn: SshConn{
-							server.Conntp.SshCn.Username,
-							server.Conntp.SshCn.Password,
-							server.Conntp.SshCn.Keypath,
-							server.Conntp.SshCn.Port,
-						},
-						TelnetCn: TelnetConn{
-							server.Conntp.TelnetCn.Username,
-							server.Conntp.TelnetCn.Password,
-							server.Conntp.TelnetCn.Port,
-						},
-					},
-					Bcktsks: BackupTasks{
-						Projbase: server.Bcktsks.Projbase,
-						//Tasks:    map[string]string{"vxflexcu": "/etc/", "xetea": "/etc/hosts"},
-						Tasks: server.Bcktsks.Tasks,
-					},
-				}
-				backupservertemp := BKServerConfigs{
-					BKSCfgs: BKServerType{
-						NFSSrv: NFSServer{
-							Ip:        bksrvyml.BKSCfgs.NFSSrv.Ip,
-							Password:  bksrvyml.BKSCfgs.NFSSrv.Password,
-							Datastore: bksrvyml.BKSCfgs.NFSSrv.Datastore,
-							Username:  bksrvyml.BKSCfgs.NFSSrv.Username,
-						},
-						SmbSrv: SambaServer{
-							Ip:        bksrvyml.BKSCfgs.SmbSrv.Ip,
-							Password:  bksrvyml.BKSCfgs.SmbSrv.Password,
-							Datastore: bksrvyml.BKSCfgs.SmbSrv.Datastore,
-							Username:  bksrvyml.BKSCfgs.SmbSrv.Username,
-						},
-					},
-				}
-				var server_itf Server
-				server_itf = &servertmp
-				servertmp.Conn()
-				servertmp.NFSMount(server_itf, &backupservertemp.BKSCfgs.NFSSrv)
-				servertmp.NFSBackup(server_itf, keys[i])
-				////////////////////////
-				wg.Done()
-			}(i)
-			wg.Wait()
-		} else {
-			time.Sleep(1 * time.Second)
-			gorNum = 0
-			i--
+		go func(k string, v LinuxOS) {
+			ch <- true
+			fmt.Println(k, v)
+			var serverItf Server
+			serverItf = &v
+			v.Conn()
+			v.NFSMount(serverItf, &bksrvyml.BKSCfgs.NFSSrv)
+			v.NFSBackup(serverItf, k)
+			<-ch
 			wg.Done()
-		}
+		}(k, v)
+	}
+	wg.Wait()
+}
+
+func linuxNfsBackup() {
+	bksrvyml, err := ReadBKServerYaml("etc/backup.yml")
+	if err != nil {
+		errors.New("yaml fmt not correct!")
+		logger.Error("yaml bkserverconfigs part fmt not correct")
 	}
 
+	srvyml, err := ReadServerYaml("etc/backup.yml")
+	if err != nil {
+		errors.New("yaml fmt not correct!")
+		logger.Error("yaml serverconfigs part fmt not correct")
+	}
+
+	wg := sync.WaitGroup{}
+
+	for prj, server := range srvyml.LinuxOsConfig {
+		wg.Add(1)
+		go func(prjname string) {
+			var server_itf Server
+			server_itf = &server
+			server.Conn()
+			server.NFSMount(server_itf, &bksrvyml.BKSCfgs.NFSSrv)
+			server.NFSBackup(server_itf, prjname)
+			wg.Done()
+		}(prj) //注意这里的值传递
+	}
+	wg.Wait()
+}
+
+func linuxNfsBackup2() {
+	bksrvyml, err := ReadBKServerYaml2("etc/backup.yml")
+	if err != nil {
+		errors.New("yaml fmt not correct!")
+		logger.Error("yaml bkserverconfigs part fmt not correct")
+	}
+
+	srvyml, err := ReadServerYaml2("etc/backup.yml")
+	if err != nil {
+		errors.New("yaml fmt not correct!")
+		logger.Error("yaml serverconfigs part fmt not correct")
+	}
+
+	wg := sync.WaitGroup{}
+
+	for prj, server := range srvyml.LinuxOsConfig {
+		wg.Add(1)
+		go func(prjname string) {
+			var server_itf Server
+			server_itf = &server
+			server.Conn()
+			server.NFSMount(server_itf, &bksrvyml.BKSCfgs.NFSSrv)
+			server.NFSBackup(server_itf, prjname)
+			wg.Done()
+		}(prj) //注意这里的值传递
+	}
+	wg.Wait()
+}
+
+func main() {
+	//linuxNfsBackup()
+	//linuxNfsBackup2()
+	linuxNfsBackupLimit(3)
 }
 
 //func linuxNfsBackupOrig() {
@@ -165,67 +179,3 @@ func linuxNfsBackup(gorlimit int) {
 //	}
 //	wg.Wait()
 //}
-
-func linuxNfsBackupOrig() {
-	bksrvyml, err := ReadBKServerYaml("etc/backup.yml")
-	if err != nil {
-		errors.New("yaml fmt not correct!")
-		logger.Error("yaml bkserverconfigs part fmt not correct")
-	}
-
-	srvyml, err := ReadServerYaml("etc/backup.yml")
-	if err != nil {
-		errors.New("yaml fmt not correct!")
-		logger.Error("yaml serverconfigs part fmt not correct")
-	}
-
-	wg := sync.WaitGroup{}
-
-	for prj, server := range srvyml.LinuxOsConfig {
-		wg.Add(1)
-		go func(prjname string) {
-			var server_itf Server
-			server_itf = &server
-			server.Conn()
-			server.NFSMount(server_itf, &bksrvyml.BKSCfgs.NFSSrv)
-			server.NFSBackup(server_itf, prjname)
-			wg.Done()
-		}(prj) //注意这里的值传递
-	}
-	wg.Wait()
-}
-
-func linuxNfsBackupOrig2() {
-	bksrvyml, err := ReadBKServerYaml2("etc/backup.yml")
-	if err != nil {
-		errors.New("yaml fmt not correct!")
-		logger.Error("yaml bkserverconfigs part fmt not correct")
-	}
-
-	srvyml, err := ReadServerYaml2("etc/backup.yml")
-	if err != nil {
-		errors.New("yaml fmt not correct!")
-		logger.Error("yaml serverconfigs part fmt not correct")
-	}
-
-	wg := sync.WaitGroup{}
-
-	for prj, server := range srvyml.LinuxOsConfig {
-		wg.Add(1)
-		go func(prjname string) {
-			var server_itf Server
-			server_itf = &server
-			server.Conn()
-			server.NFSMount(server_itf, &bksrvyml.BKSCfgs.NFSSrv)
-			server.NFSBackup(server_itf, prjname)
-			wg.Done()
-		}(prj) //注意这里的值传递
-	}
-	wg.Wait()
-}
-
-func main() {
-	//linuxNfsBackup(3)
-	//linuxNfsBackupOrig()
-	linuxNfsBackupOrig2()
-}
